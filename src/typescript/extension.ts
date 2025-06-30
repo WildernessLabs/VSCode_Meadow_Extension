478d4bf01556adeb7efc501ec0b9547db11788a2
CartBlanche <savagesoftware@gmail.com>
2025-06-30 16:55:11 +0100

    Fix fact I'd stopped the meadow:build task from being called :facepalm:

--- src/typescript/extension.ts
+++ src/typescript/extension.ts
@@ -31,9 +31,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	this.MeadowProjectManager = new MeadowProjectManager(context);
 
-	if (MeadowProjectManager.Shared.HasSupportedProjects) {
-		this.meadowBuildTaskProvider = vscode.tasks.registerTaskProvider(MeadowBuildTaskProvider.MeadowBuildScriptType, new MeadowBuildTaskProvider(context));
-	}
+	this.meadowBuildTaskProvider = vscode.tasks.registerTaskProvider(MeadowBuildTaskProvider.MeadowBuildScriptType, new MeadowBuildTaskProvider(context));
 	
 	//context.subscriptions.push(vscode.commands.registerCommand('extension.meadow.configureExceptions', () => configureExceptions()));
 	context.subscriptions.push(vscode.commands.registerCommand(
